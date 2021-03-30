4ebd575dae31cfb3b5b209f44bc9129b600ce781
Antoine SCAVINER <20521@ecam.be>
2021-03-30 17:48:39 +0200

    pong ?

--- client.py
+++ client.py
@@ -6,39 +6,51 @@ import json
 
 def main(argv):
     assert(type(argv)) is list,str(type(argv))+ "got, exoected <list>"
-    port=argv[0]
+    port=int(argv[1])
     socket = s.socket()
-    socket.connect((("127.0.0.1",3000)))
-    msg = {"request":"subscribe","port":"3000","name":"toto","matricules":"20521"}
-    msg = json.dumps(msg)
-    msg = msg.encode("utf8")
-    total = 0
-    while total < len(msg):
-        sent = socket.send(msg[total:])
-        total += sent
-    #sendJSON(socket,{'request':'subscribe','subscribe':'subscribe'})
-    #connect(s,port)       #connect to server
-    #askSubscribe(s,port)   #subscribe to server
-    pass
+    outComingAddress = ("127.0.0.1",port)
+    inComingaddress = ("127.0.0.1",3000)
+    connect(socket,outComingAddress)
+    askSubscribe(socket,port)
+    toPrint = ping(inComingaddress)
 
-def connect(s,port):
+def connect(s,address):
     # connect to server
     try:    
-        s.connect(('localhost',port))
+        s.connect(address)
     except :
         print("connection failed")
 
 def askSubscribe(client,port):
     # subscribe to server
-    msg = "{subscribe}".encode('utf8')
+    msg = {"request":"subscribe","port":port,"name":"toto","matricules":"20521"}
+    msg = json.dumps(msg)
+    msg = msg.encode("utf8")
+    total = 0
     try :
-        client.sendto(msg, ('localhost',port))
-        sendJSON(client,{['suscribe']})
+        while total < len(msg):
+            sent = client.send(msg[total:])
+            total += sent
     except :
         print("subscribtion failed")
 
+def ping(address, timeout=1):
+    '''
+    Request response from address. Data is included in the request
+    '''
+    socket = s.socket()
+    socket.connect(address)
+    msg = {"marbles": [[1, 1], [2, 2]],"direction": "SE"}
+    msg = json.dumps(msg)
+    msg = msg.encode("utf8")
+    socket.send(msg)
+    response = receiveJSON(socket, timeout)
+    print(response)
+    return response
+
 def stopSubscriptions():
     pass
+
 if (__name__=="__main__"):
     main(sys.argv)
 
