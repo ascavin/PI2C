560e4a980b553aff19b7e5810e77ca7144b60b4d
Antoine SCAVINER <20521@ecam.be>
2021-03-30 16:56:02 +0200

    add connection & subscribe

--- client.py
+++ client.py
@@ -1,15 +1,24 @@
 import pygame
-import socket
+import socket as s
+from jsonNetwork import Timeout, sendJSON, receiveJSON, NotAJSONObject, fetch
 import sys
+import json
 
 def main(argv):
     assert(type(argv)) is list,str(type(argv))+ "got, exoected <list>"
-    current_state=["INIT"]
     port=argv[0]
-    s = socket.socket()
-    connect(s,port)       #connect to server
-    AskSubscribe(s,port)   #subscribe to server
-    current_state=["WAIT"]
+    socket = s.socket()
+    socket.connect((("127.0.0.1",3000)))
+    msg = {"request":"subscribe","port":"3000","name":"toto","matricules":"20521"}
+    msg = json.dumps(msg)
+    msg = msg.encode("utf8")
+    total = 0
+    while total < len(msg):
+        sent = socket.send(msg[total:])
+        total += sent
+    #sendJSON(socket,{'request':'subscribe','subscribe':'subscribe'})
+    #connect(s,port)       #connect to server
+    #askSubscribe(s,port)   #subscribe to server
     pass
 
 def connect(s,port):
@@ -17,54 +26,19 @@ def connect(s,port):
     try:    
         s.connect(('localhost',port))
     except :
-        print("connection failed")   
+        print("connection failed")
 
-def AskSubscribe(s,port):
+def askSubscribe(client,port):
     # subscribe to server
     msg = "{subscribe}".encode('utf8')
     try :
-        s.sendto(msg, ('localhost',port))
+        client.sendto(msg, ('localhost',port))
+        sendJSON(client,{['suscribe']})
     except :
         print("subscribtion failed")
 
-def incomingMessage():
-    '''
-		Route request to request handlers
-	'''
-	print('request from', address)
-	try:
-		request = receiveJSON(client)
-		
-		if request['request'] == 'subscribe':
-			startSubscription(client, address, request)
-		else:
-			raise ValueError('Unknown request \'{}\''.format(request['request']))
-
-	except Timeout:
-		sendJSON(client, {
-			'response': 'error',
-			'error': 'transmition take too long'
-		})
-	except NotAJSONObject as e:
-		sendJSON(client, {
-			'response': 'error',
-			'error': str(e)
-		})
-	except KeyError as e:
-		sendJSON(client, {
-			'response': 'error',
-			'error': 'Missing key {}'.format(str(e))
-		})
-	except Exception as e:
-		sendJSON(client, {
-			'response': 'error',
-			'error': str(e)
-		})
-
-def oucomingMessage():
-
-
+def stopSubscriptions():
+    pass
 if (__name__=="__main__"):
-    if(len.sys.argv>1)
-        main(sys.argv)
-    else(main(sys.argv+["4000"]))
+    main(sys.argv)
+
