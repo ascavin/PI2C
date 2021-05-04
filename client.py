5775c3b4c819fd23a4f6caad509c1d5dd82a2b61
Antoine SCAVINER <20521@ecam.be>
2021-05-04 17:24:45 +0200

    improving structure

--- client.py
+++ client.py
@@ -4,81 +4,66 @@ from threading import Thread
 import sys
 import json
 import time
-import movetest
+import movetest as mv
+from collections import defaultdict
+import random
+import negamax
 
 class game:
-
-    __current_state = "initialize"
-    __name = "toto"
-    __port_client = 3001
-    __port_server = 3000
-    __host = "127.0.0.1"
-
+    def __init__(self,portClient,name='toto',portServer=3000):
+        self.__name = name
+        self.__port_client = int(portClient)
+        self.__port_server = portServer
+        self.__host = "127.0.0.1"
+        self.__live = 0
+    
     def subscribe(self):
-
-    def isMyTurn(self,data):
-        if (data['players'][data["current"]]==self.__name):
-            pass
+        server_socket = socket.socket()                           # instantiate
+        server_socket.connect((self.__host, self.__port_server))  # connect to the server
+        sendJSON(server_socket,{"request":"subscribe","port":self.__port_client,"name":self.__name,"matricules":"20521"})
+        subscription=True
+        while subscription:
+            data = receiveJSON(server_socket)
+            print(data)
+            if (data['response']=="ok"):
+                subscription=False  
+                return True
         return False
-
-    def checkLife(self,data):
-        if (data['lives']>0):
-            return True
-        else : 
-            return False
-
-
-
-def main(argv):
-    assert(type(argv)) is list,str(type(argv))+ "got, exoected <list>"
-    port_client=int(argv[1])
-    name=argv[2]
-    port_server = 3000  # socket server port number
-    host = "127.0.0.1"
-    server_socket = socket.socket()  # instantiate
-    server_socket.connect((host, port_server))  # connect to the server
-    sendJSON(server_socket,{"request":"subscribe","port":port_client,"name":name,"matricules":"20521"})
-    subscription=True
-    while subscription:
-        data = receiveJSON(server_socket)
-        print(data)
-        if (data['response']=="ok"):
-            subscription=False
-    ping=True
-    client_socket = socket.socket()                # Create a socket object
-    client_socket.bind((host, port_client))        # Bind to the port
-    client_socket.listen(5)
-    while ping:
-        print("try")
-        c,addr = client_socket.accept()
-        print('Got connection from', addr)    
-        data=receiveJSON(c)
-        if (data['request']=='ping'):
-            sendJSON(c,{'response':'pong'})
-        elif (data['request']=='play'):
-            print("data state :",data['state'])
-            print("data['state']['current'] :",data['state']["current"])
-            print("data['state']['players'][data['state']['current']] :",data['state']['players'][data['state']['current']])
-            break
-            # 
-            # #isMyTurn(state)
-            # readGridState()
-            # computeMove(data['state'])
+    
+    def app(self):
+        self.subscribe()
+        ping=True
+        client_socket = socket.socket()                              # Create a socket object
+        client_socket.bind((self.__host, self.__port_client))        # Bind to the port
+        client_socket.listen(5)
+        while ping:
+            print("try")
+            c,addr = client_socket.accept()
+            print('Got connection from', addr)    
+            data=receiveJSON(c)
+            if (data['request']=='ping'):
+                sendJSON(c,{'response':'pong'})
+            elif (data['request']=='play'):
+                nextMove=self.play(data)
+                print(nextMove)
+                break
         c.close()
 
+    def move(self,state):
+        nextMove=negamax.run(state)
+        return nextMove
 
+    def play(self,data):
+        if data['lives']>=1:
+            nextMove=self.move(data['state'])
+        return nextMove
+    
+    
+        
 
-def readGridState():
-    return False
-
-def computeMove(grid):
-    checkMove(grid)
-    return False
-
-def checkMove(grid):
-    return False
-
-
+def main(argv):
+    player=game(argv[1],argv[2])
+    player.app()
 
 if (__name__=="__main__"):
     main(sys.argv)
---
+++ negamax.py
@@ -0,0 +1,193 @@
+from collections import defaultdict
+import random
+import time 
+import movetest_copy as find
+
+state = [ 
+    None,None,None,
+    None,None,None,
+    None,None,None,
+]
+lines = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+
+def winner(state):
+	for line in lines:
+		values = set((state[i] for i in line))
+		if len(values) == 1:
+			player = values.pop()
+			if player is not None:
+				return player
+	return None
+
+def utility(state, player):
+	theWinner = winner(state)
+	if theWinner is None:
+		return 0
+	if theWinner == player:
+		return 1
+	return -1
+
+def gameOver(state):
+	if winner(state) is not None:
+		return True
+
+	empty = 0
+	for elem in state:
+		if elem is None:
+			empty += 1
+	return empty == 0
+
+def currentPlayer(state):
+	counters = {1: 0, 2: 0, None: 0}
+	for elem in state:
+		counters[elem] += 1
+	
+	if counters[1] == counters[2]:
+		return 1
+	return 2
+
+def moves(state):               
+	res = []
+	for i, elem in enumerate(state):
+		if elem is None:
+			res.append(i)
+	
+	random.shuffle(res)
+	return res
+
+def apply(state, move):            #to change state of grid use papply
+	player = currentPlayer(state)
+	res = list(state)
+	res[move] = player
+	return res
+
+def timeit(fun):
+	def wrapper(*args, **kwargs):
+		start = time.time()
+		res = fun(*args, **kwargs)
+		print('Executed in {}s'.format(time.time() - start))
+		return res
+	return wrapper
+
+@timeit
+def next(state,fun):
+	player = currentPlayer(state)
+	_, move = fun(state, player)
+	return move
+
+def show(state):
+	state = [
+            'X' if val == 1 else 
+			'O' if val == 2 else 
+			' ' for val in state]
+	print(state[:3])
+	print(state[3:6])
+	print(state[6:])
+	print()
+
+def lineValue(line, player):
+	counters = {
+		1: 0,
+		2: 0,
+		None: 0
+	}
+
+	for elem in line:
+		counters[elem] += 1
+
+	if counters[player] > counters[player%2+1]:
+		return 1
+	if counters[player] == counters[player%2+1]:
+		return 0
+	return -1
+
+
+def heuristic(state, player):
+	if gameOver(state):
+		theWinner = winner(state)
+		if theWinner is None:
+			return 0
+		if theWinner == player:
+			return 9
+		return -9
+	res = 0
+	for line in lines:
+		res += lineValue([state[i] for i in line], player)
+	return res
+
+from collections import defaultdict
+
+def negamaxWithPruningIterativeDeepening(state, player, timeout=0.2):
+	cache = defaultdict(lambda : 0)
+	def cachedNegamaxWithPruningLimitedDepth(state, player, depth, alpha=float('-inf'), beta=float('inf')):
+		over = gameOver(state)
+		if over or depth == 0:
+			res = -heuristic(state, player), None, over
+
+		else:
+			theValue, theMove, theOver = float('-inf'), None, True
+			possibilities = [(move, apply(state, move)) for move in moves(state)]
+			possibilities.sort(key=lambda poss: cache[tuple(poss[1])])
+			for move, successor in reversed(possibilities):
+				value, _, over = cachedNegamaxWithPruningLimitedDepth(successor, player%2+1, depth-1, -beta, -alpha)
+				theOver = theOver and over
+				if value > theValue:
+					theValue, theMove = value, move
+				alpha = max(alpha, theValue)
+				if alpha >= beta:
+					break
+			res = -theValue, theMove, theOver
+		cache[tuple(state)] = res[0]
+		return res
+
+	value, move = 0, None
+	depth = 1
+	start = time.time()
+	over = False
+	while value > -9 and time.time() - start < timeout and not over:
+		value, move, over = cachedNegamaxWithPruningLimitedDepth(state, player, depth)
+		depth += 1
+
+	print('depth =', depth)
+	return value, move
+
+
+def run(state):
+	print(state)
+	# state = [
+	# 	None, None, None,
+	# 	None, None, None,
+	# 	None, None, None,
+	# ]
+
+	# show(state)
+	# while not gameOver(state):
+	# 	move = next(state, negamaxWithPruningIterativeDeepening)
+	# 	state = apply(state, move)
+	# 	show(state)
+	def getMarbleLocation(state,symbol):
+		locations=[] 
+		for i,line in enumerate(state['board']):
+			for e,column in enumerate(line) :
+				if (state['board'][i][e]==symbol):
+					locations.append((i,e))
+		return locations
+	marbles = getMarbleLocation(state,'B')
+	possibleMoves = []
+	for marble in marbles:
+		possibleMoves.append(find.moveaMarbleispossible(state,marble))
+	index=random.randint(0,len(possibleMoves))
+	return possibleMoves[index]
+
+
+
+
