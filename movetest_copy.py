c25b62f7adf09fa8b88c3d79fd9cd61e519dd05f
Matthieu Faget <20517@ecam.be>
2021-05-04 17:54:19 +0200

    modifcopy

--- movetest_copy.py
+++ movetest_copy.py
@@ -139,7 +139,7 @@ def boucleangleonemarble(li,ci):
 				dictio[elem] = (xi,yi)
 
 		pass
-	elif li == 8 and ci ==4:
+	elif li == 8 and ci ==8:
 		print("8 and 4")
 		for elem in ["W","NW","NE"]:
 			#print(mvt.addDirection((li,ci),elem))
@@ -147,8 +147,29 @@ def boucleangleonemarble(li,ci):
 			if state['board'][xi][yi] == "E":
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
-				dictio[elem] = (xi,yi)			
+				dictio[elem] = (xi,yi)	
+
+	elif li == 0 and ci ==0:
+		print("0 and 0")
+		for elem in ["SW","SE","E"]:
+			#print(mvt.addDirection((li,ci),elem))
+			xi,yi=mvt.addDirection((li,ci),elem)
+			if state['board'][xi][yi] == "E":
+				isitp = True
+				#result.append([mvt.addDirection((li,ci),elem),elem])
+				dictio[elem] = (xi,yi)		
 		pass
+
+	elif li == 8 and ci ==4:
+		print("8 and 4")
+		for elem in ["NW","NE","E"]:
+			#print(mvt.addDirection((li,ci),elem))
+			xi,yi=mvt.addDirection((li,ci),elem)
+			if state['board'][xi][yi] == "E":
+				isitp = True
+				#result.append([mvt.addDirection((li,ci),elem),elem])
+				dictio[elem] = (xi,yi)
+
 	elif li == 0 and ci == 4:
 		print("0 and 4")
 		for elem in ["W","SW","SE"]:
@@ -264,10 +285,9 @@ def isnotOnBoard(pos):
 		return True
 	return False
 
-def boucleangle2marble(state,li,ci):
+def bouclestayonboard2marble(state,li,ci):
 	Opponent = mvt.opponent(mvt.symbols[state['current']])
 	Allie = mvt.symbols[state['current']]
-	#print(Allie)
 	#result = []
 	dictio = {}
 	isitp = False
@@ -280,13 +300,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass
-
+			
 		pass
 	elif li == 8 and ci ==4:
 		print("8 and 4")
@@ -297,12 +311,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass			
+					
 		pass
 	elif li == 0 and ci == 4:
 		print("0 and 4")
@@ -313,13 +322,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)			
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass
-		
+					
 		pass
 	elif li == 4 and ci == 8:
 		print("4 and 8")
@@ -330,12 +333,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)	
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass					
+							
 		pass
 	elif 4<li<8 and 0<ci<4:
 		print("48 and 04")
@@ -346,12 +344,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass			
+					
 		pass
 	elif 4<li<8 and 4<ci<8:
 		print("48 and 04")
@@ -362,12 +355,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)	
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass		
+				
 		pass	
 	elif 0<li<4 and 4<ci<8:
 		print("04 and 48")
@@ -378,12 +366,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass			
+					
 		pass
 	elif li==0 and 0<ci<4:
 		print("0 and 04")
@@ -394,12 +377,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)	
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass		
+				
 		pass
 	elif 0<li<4 and ci==0:
 		print("04 and 0")
@@ -410,12 +388,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)	
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass		
+				
 		pass
 	elif 4<li<8 and ci==8:
 		print("48 and 8")
@@ -426,12 +399,7 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)	
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass		
+				
 		pass
 	elif li==8 and 4<ci<8:
 		print("8 and 48")
@@ -442,12 +410,6 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass
 		pass
 	
 	else : 
@@ -459,48 +421,122 @@ def boucleangle2marble(state,li,ci):
 				isitp = True
 				#result.append([mvt.addDirection((li,ci),elem),elem])
 				dictio[elem] = (xi,yi)
-			elif state['board'][xi][yi] == Opponent:# si il y a un adversaire
-				xo,yo = mvt.addDirection((xi,yi),elem)
-				if state['board'][xo][yo] =="E" or state['board'][xo][yo] =="X" or  isnotOnBoard((xo,yo)):# si il n'y a rien derriere 
-					dictio[elem] = (xi,yi)
-					isitp = True
-					pass
-
+			
 		pass
 	#result.insert(0,isitp)
 	return isitp,dictio
 
-def boucle2marbles(dictio1,dictio2):
+def boucle2marblesexceptionmove(dictio1,dictio2,pos1,pos2):# mouvements possible pour les 2 billes
 	dict2marble = {}
-	#print(dictio1)
-	#print(dictio2)
+	print(dictio1)
+	print(dictio2)
+	x1,y1 = pos1
+	x2,y2 = pos2
+	#print(pos2)
 	statevar3 = False
 	for elem in dictio1:		
 		for otherelem in dictio2:
-			#print(dictio1[elem])
-			#print(dictio2[otherelem])
-			if elem == otherelem:
+			
+			if elem == otherelem: # si 2 deplacements en commun ( deplacement en parallele)
 				#print("ok")
 				dict2marble[elem] = dictio1[elem]
 				statevar3 = True
+
+			# deplacements en serie 
+			elif (dictio2[otherelem][0],dictio2[otherelem][1]-2) == pos1 or (dictio2[otherelem][0],dictio2[otherelem][1]+2) == pos1 :#axe des y vu du voisin
+				dict2marble[otherelem] = dictio2[otherelem]
+				#print("other",elem,dictio2[otherelem],otherelem)
+				pass
+
+			elif (dictio2[otherelem][0]-2,dictio2[otherelem][1]) == pos1 or (dictio2[otherelem][0]+2,dictio2[otherelem][1]) == pos1 :#axe des x vu du voisin
+				dict2marble[otherelem] = dictio2[otherelem]
+				pass
+			elif (dictio1[elem][0],dictio1[elem][1]+2) == pos2 or (dictio1[elem][0],dictio1[elem][1]-2) == pos2 : # axe des y vue de moi
+				dict2marble[elem] = dictio1[elem]
+				#print("me",elem,dictio1[elem])
+				pass
+			elif (dictio1[elem][0]+2,dictio1[elem][1]) == pos2 or (dictio1[elem][0]-2,dictio1[elem][1]) == pos2 :# axes des x vu de moi
+				dict2marble[elem] = dictio1[elem]
+				pass
+
+			# en diagonale 
+			elif (dictio2[otherelem][0]-2,dictio2[otherelem][1]-2) == pos1 or (dictio2[otherelem][0]+2,dictio2[otherelem][1]+2) == pos1 :#diag vu du voisin
+				dict2marble[otherelem] = dictio2[otherelem]
+				#print("other",elem,dictio2[otherelem],otherelem)
+				pass
+			elif (dictio1[elem][0]+2,dictio1[elem][1]+2) == pos2 or (dictio1[elem][0]-2,dictio1[elem][1]-2) == pos2 : #diag vue de moi
+				dict2marble[elem] = dictio1[elem]
+				#print("me",elem,dictio1[elem])
+				pass	
+
 	return statevar3,dict2marble
+
+def moveennemi(state,pos,dictio,currentmarble):
+	li,ci= pos
+	opponentplayer = mvt.opponent(currentmarble)
+	for elem in dictio:# pour chaque alignement
+		elemopo = opposite[elem]
+		# je veux regarder sur la ligne si il ny a pas dadversaire 
+		xv,yv=mvt.addDirection((li,ci),elem)#coordonne du voisin 
+		xvp1,yvp1 = mvt.addDirection((xv,yv),elem)#1 case apres le voisin
+		xvp2,yvp2 = mvt.addDirection((xvp1,yvp1),elem)# 2 cases apres le voisin
+		xim1,yim1 = mvt.addDirection((li,ci),elemopo)#1case avant moi direction oppose
+		xim2,yim2 = mvt.addDirection((xim1,yim1),elemopo)#2 cases avant moi direction oppose
+		#print("xv,yv",xv,yv,"xvp1,yvp1",xvp1,yvp1,"xvp2,yvp2",xvp2,yvp2,"xim1,yim1",xim1,yim1,"xim2,yim2",xim2,yim2)
+		
+		if state['board'][xvp1][yvp1] == opponentplayer and state['board'][xvp2][yvp2] == "E":
+			dictio[elem][elem] = (xv,yv)
+			print("ok",dictio[elem])
+			pass
+
+		elif state['board'][xim1][yim1] == opponentplayer and state['board'][xim2][yim2] == "E":
+			#dictio[elem][elem] = (xim1,yim1)
+			pass
+		#state['board'][x][y]
+		#print(dictio)
+
+	return False
 
 def moveaMarbleispossible(state, pos):
 	li, ci = pos
 	return boucleangleonemarble(li, ci)
 
-def move2Marbleispossible(state,pos1,pos2):
-	l1,c1 = pos1
-	l2,c2 = pos2
-	statevar1,dictio1 = boucleangle2marble(state,l1, c1)
-	statevar2,dictio2 = boucleangle2marble(state,l2, c2)
-	statevar3,dictio12 = boucle2marbles(dictio1,dictio2)
+def move2Marbleispossible(state,pos):
+	l1,c1 = pos#possition of the marble
+	statevarneighbour = False
+	dictio13 = {}
+	Allie = mvt.symbols[state['current']]# get allie symbole
+	if mvt.findNeighbor(state['board'],(l1,c1),Allie,True)[0] :# si il y a voisin
+		for elem in list(mvt.findNeighbor(state['board'],(l1,c1),Allie,True)[1].keys()): # regarde pour chaque direction des voisins
+			xn,yn = mvt.findNeighbor(state['board'],(l1,c1),Allie,True)[1][elem]		#recupere les coordonnes de chaque voisin
 
-	return statevar3,dictio12
+			statevar1,dictio1 = bouclestayonboard2marble(state,l1,c1)# mouvement possible pour ma bille 
+			statevar2,dictio2 = bouclestayonboard2marble(state,xn,yn)# mouvement possible pour mon voisin allie
+			#print(dictio1)
+			#print(dictio2)
+			pos1 = l1,c1
+			pos2 = xn,yn
+			#print(pos2)
+			statevar3,dictio12 = boucle2marblesexceptionmove(dictio1,dictio2,pos1,pos2)# mouvements possible pour les 2 billes
+			dictio13[elem] = dictio12
+		pass
+	else :
+		return statevarneighbour 
+	moveennemi(state,pos,dictio13,Allie)
+
+	return statevar3,dictio13
 
 def canImoveamarblenexttoallie(state,pos):
 	return False
 
+opposite = {
+	'NE': 'SW',
+	'SW': 'NE',
+	'NW': 'SE',
+	'SE': 'NW',
+	'E': 'W',
+	'W': 'E'
+}
 
 if __name__=='__main__':
 	def show(state):
@@ -508,14 +544,20 @@ if __name__=='__main__':
 		print()
 	state, next = mvt.Abalone(["jojo","jack"])
 	#print(state)
-	state['board'][4][3] = 'B'
+	#state['board'][4][3] = 'B'
 	state['board'][4][4] = 'B'
-	#state['board'][4][5] = 'W'
-	#state['board'][4][6] = 'W'
+	state['board'][4][5] = 'B'
+	state['board'][4][6] = 'W'
+	state['board'][4][3] = 'W'
+	state['board'][5][4] = 'B'
 	show(state)
 	#moveaMarbleispossible(state, (4,4))
-	#print(moveaMarbleispossible(state, (1,4)))
-	print(move2Marbleispossible(state,(4,3),(4,4)))
+	#print(moveaMarbleispossible(state, (4,3)))
+	#print(mvt.findNeighbor(state['board'],(4,3),"W",True))	
+	#print(mvt.findNeighbor(state['board'],(4,3),"W",True))
+	print(move2Marbleispossible(state,(4,4)))
+	#print(list(mvt.findNeighbor(state['board'],(4,4),'B',True)[1].keys()))
+	#print(mvt.findNeighbor(state['board'],(4,4),'B',True)[1]['NW'])
 
 
 
