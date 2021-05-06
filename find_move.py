01e29839b15ac7d6fe12317520254a4c84258948
Matthieu Faget <20517@ecam.be>
2021-05-06 11:23:36 +0200

    findmove

--- find_move.py
+++ find_move.py
@@ -7,6 +7,21 @@ directions = {
 		'E': ( 0,  1),
 		'W': ( 0, -1)
 	}
+
+opposite = {
+	'NE': 'SW',
+	'SW': 'NE',
+	'NW': 'SE',
+	'SE': 'NW',
+	'E': 'W',
+	'W': 'E'
+}
+
+def opponent(color):
+	if color == 'W':
+		return 'B'
+	return 'W'
+
 def findMove(grid,marble,symbol):
 	moves=[]
 	alignement=[]
@@ -16,15 +31,15 @@ def findMove(grid,marble,symbol):
 		find=True
 		e=1        #nombre d'élément de l'alignement
 		while find and e<5:           
-			print("neighbor",(marble[0]+directions[direction][0]*e,marble[1]+directions[direction][1]*e))
+			#print("neighbor",(marble[0]+directions[direction][0]*e,marble[1]+directions[direction][1]*e))
 			alignement.append(neighbor(grid,(marble[0]+directions[direction][0]*e,marble[1]+directions[direction][1]*e)))
 			possibilities=getPossibilities(symbol)
 			for i,possibilitie in enumerate(possibilities):
 				#print(alignement)
 				#print("-------------------------")      
 				if possibilitie == alignement :
-					print("possibilitie",possibilitie)
-					print("alignement",alignement)
+					#print("possibilitie",possibilitie)
+					#print("alignement",alignement)
 					alignement.clear()
 					find=False
 					if i == 0: 
@@ -54,41 +69,38 @@ def findMove(grid,marble,symbol):
 
 			
 def neighbor(grid,marble):
-	#print(marble)
-	if  marble[0]>=0 and marble[0]<8 and marble[1]<8 and marble[1]>=0:
-		res=grid[marble[0]][marble[1]]
-		if res=='X':
-			res='E'
-		return res
-	else : return 'E'
+	li,ci = marble
 
-def getPossibilities(symbol):
-		if symbol == 'W':         
-			possibilities =[
-							['E'],                 #:True,    #0    #je sors une de mes billes si W en bordure ou E à l'exterieur du plateau
-							['W','E'],             #:True,    #1    #je sors une de mes billes si W en bordure ou E à l'exterieur du plateau
-							['W','W','E'],         #:True,    #2    #je sors une de mes billes si W en bordure ou E à l'exterieur du plateau
-							['W','B','E'],         #:True,    #3    #je sors une bille adverse
-							['W','B','B'],         #:False,   #4    #je ne peux pas bouger
-							['W','W','W'],         #:False,   #5    #je ne peux pas bouger
-							['W','W','B','E'],     #:True,    #6    #je sors une bille adverse
-							['W','W','B','B','E'], #:True,    #7    #je sors une bille adverse
-							['W','W','B','B','B'], #:False,   #8    #je ne peux pas bouger
-							]
-			return possibilities
-		else :
-			possibilities =[
-							['E'],                 #:True,    #0
-							['B','E'],             #:True,    #1
-							['B','B','E'],         #:True,    #2
-							['B','W','E'],         #:True,    #3
-							['B','W','W'],         #:False,   #4
-							['B','B','B'],         #:False,   #5
-							['B','B','W','E'],     #:True,    #6
-							['B','B','W','W','E'], #:True,    #7
-							['B','B','W','W','W'], #:False,   #8
-							]
-			return possibilities
+	print(marble)
+	if  (0<=li<=4 and 0<=ci<=4) or (5<=li<=8 and 5<=ci<=8) or (5<=li<=8 and 0<=ci<=4 and not grid[li][ci] == 'X') or (0<=li<=4 and 5<=ci<=8 and not grid[li][ci] == 'X'):
+		res=grid[marble[0]][marble[1]]
+		return res
+	else : 
+		return 'X'
+
+def getPossibilities(symbol):         
+	possibilities =[
+					['E'],                 #:True,    #0    #je sors une de mes billes si W en bordure ou E à l'exterieur du plateau
+					[symbol,'E'],             #:True,    #1    #je sors une de mes billes si W en bordure ou E à l'exterieur du plateau
+					[symbol,symbol,'E'],         #:True,    #2    #je sors une de mes billes si W en bordure ou E à l'exterieur du plateau
+					[symbol,opponent(symbol),'E'],         #:True,    #3    #je sors une bille adverse
+					[symbol,opponent(symbol),opponent(symbol)],         #:False,   #4    #je ne peux pas bouger
+					[symbol,symbol,symbol],         #:False,   #5    #je ne peux pas bouger
+					[symbol,symbol,opponent(symbol),'E'],     #:True,    #6    #je sors une bille adverse
+					[symbol,symbol,opponent(symbol),opponent(symbol),'E'], #:True,    #7    #je sors une bille adverse
+					[symbol,symbol,opponent(symbol),opponent(symbol),opponent(symbol)], #:False,   #8    #je ne peux pas bouger
+					]
+
+					#['E'],                 #:True,    #0
+					#['B','E'],             #:True,    #1
+					#['B','B','E'],         #:True,    #2
+					#['B','W','E'],         #:True,    #3
+					#['B','W','W'],         #:False,   #4
+					#['B','B','B'],         #:False,   #5
+					#['B','B','W','E'],     #:True,    #6
+					#['B','B','W','W','E'], #:True,    #7
+					#['B','B','W','W','W'], #:False,   #8
+	return possibilities
 
 if __name__=='__main__':  
 	grid=[
@@ -105,7 +117,14 @@ if __name__=='__main__':
 	def show(state):
 		print('\n'.join([' '.join(line) for line in state]))
 		print()
-	result=findMove(grid,(8,8),'B')
+	
+	grid[4][0] = 'W'
+	grid[4][1] = 'B'
+	grid[4][2] = 'B'
+	grid[4][3] = 'W'
+	grid[3][1] = 'W'
+
+	result=findMove(grid,(4,2),'B')
 	show(grid)
 	print(result)
 
